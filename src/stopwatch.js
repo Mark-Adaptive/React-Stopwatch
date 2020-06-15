9343f1e61cbbc34d5d7271c2cf0bff8476dab48e
Mark Bekker <mark.bekker@weareadaptive.com>
2020-06-14 21:36:58 -0400

    Fully implemented lap feature

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,85 +1,104 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
+function formatTime(inputTime) {
+    const rawTime = new Date(inputTime);
+    const minutes = rawTime.getMinutes().toString().padStart(2, '0');
+    const seconds = rawTime.getSeconds().toString().padStart(2, '0');
+    const centis = Math.round(rawTime.getMilliseconds() /10).toString().padStart(2, '0');
+  
+    return `${minutes} : ${seconds} . ${centis}`;
+}
 
 function Stopwatch() {
     const [time, setTime] = useState(0);
     const [isOn, setIsOn] = useState(false);
-    const [isLap, setIsLap] = useState(false);
-    const [prevTime, setPrevTime] = useState(0);
-    const [prevLapTime, setPrevLapTime] = useState(0); 
-    const [shortestLap, setShortestLap] = useState(null);
-    const [longestLap, setLongestLap] = useState(null);
+    const [laps, setLaps] = useState([]);
 
     const interval = useRef(null);
-    const resetLapButtonDisplay = useRef("Reset");
-    const startStopButtonDisplay = useRef("Start");
-    function flipToggleButton() {
-       isOn ? stop() : start();
-    }
+    const startTime = useRef(0);
+    const startLapTime = useRef(0);
+    const shortestLap = useRef(null);
+    const longestLap = useRef(null);
+    const lapDisplay = useRef([]);
+    const totalLapTime = useRef(0); 
 
-    function flipLapButton() {
-        isOn ? (!isLap ? startLap(): stopLap()) : reset();
-    }
-
-    function updateTime() {
-        if (prevTime == 0){
-            setPrevTime(Date.now());
+    //Increment clock on start and don't increment on stop
+    useEffect(() => {
+        if (isOn) {
+            startTime.current += Date.now();
+            interval.current = setInterval(() => {
+                setTime(Date.now()-startTime.current);
+            }, 10);
+            return () => {
+                clearInterval(interval.current);
+                startTime.current -= Date.now();
+            }
         }
-        let currentTime = Date.now();
-        let timePassed = currentTime - prevTime;
-        setPrevTime(currentTime);
-        setTime(time+timePassed);
-    }
-    function start() {
-        interval.current = setInterval(()=>updateTime(), 10);
-        let currentTime = Date.now();
-        setIsOn(true);
-        setPrevLapTime(currentTime);
-        resetLapButtonDisplay.current = "Lap";
-        startStopButtonDisplay.current = "Stop";
-    }
-    
-    function stop() {
-        setIsOn(false);
-        resetLapButtonDisplay.current = "Reset";
-        startStopButtonDisplay.current = "Start";
-        clearInterval(interval.current);
-    }
+    }, [isOn])
 
     function reset() {
-        setIsOn(false);
-        setShortestLap(null);
-        setLongestLap(null);
         setTime(0);
-        setPrevTime(0);
-    }
-    
-    function startLap() {
-
-    }
-
-    function stopLap() {
-
+        setLaps(lapsList => []);
+        startTime.current = 0;
+        startLapTime.current = 0;
+        shortestLap.current = null; //In milliseconds
+        longestLap.current = null; //In milliseconds
+        totalLapTime.current = 0;
     }
 
-    function formatTime(inputTime) {
-        const rawTime = new Date(inputTime);
-        const minutes = rawTime.getMinutes().toString().padStart(2, '0');
-        const seconds = rawTime.getSeconds().toString().padStart(2, '0');
-        const centis = Math.round(rawTime.getMilliseconds() /10).toString().padStart(2, '0');
-      
-        return `${minutes} : ${seconds} . ${centis}`;
+    function flipToggle(){
+        setIsOn(prevState => !prevState);
     }
+
+    function lap() {
+        //Calculate time of new lap and set timer for next lap
+        let lapTime;
+        laps.length !== 0 ? lapTime = time - totalLapTime.current : lapTime = time; //Subtract current total lap time from time display time for new lap time
+        //Push new lap to lap array
+        setLaps(lapsList => [...lapsList, lapTime]);
+    }
+
+    //Update shortest and longest laps on addition of new lap to array 
+    useEffect(() => {
+        if (laps.length === 1){
+            shortestLap.current = laps[0];
+            longestLap.current = laps[0];
+        } else if (laps.length > 1) {
+            for (let i=0; i < laps.length; i++){
+                if (shortestLap.current >= laps[i]) {
+                    shortestLap.current = laps[i];
+                }
+                else if (longestLap.current <= laps[i]) {
+                    longestLap.current = laps[i];
+                }
+            }
+        }
+
+        //Update display laps to have shortest lap in green and longest lap in red
+        lapDisplay.current = [];
+        for (let i=0; i < laps.length; i++) {
+            if (longestLap.current === laps[i] && laps.length > 1) {
+                lapDisplay.current.push(<li style={{ color: 'red' }} key={i}>{formatTime(laps[i])}</li>);
+            } else if (shortestLap.current === laps[i] && laps.length > 1) {
+                lapDisplay.current.push(<li style={{ color: 'green' }} key={i}>{formatTime(laps[i])}</li>);
+            } else {
+                lapDisplay.current.push(<li style={{ color: 'black' }} key={i}>{formatTime(laps[i])}</li>);
+            }
+        }
+        //Update total lap time
+        totalLapTime.current = laps.reduce((a,b) => a + b, 0);
+    }, [laps])
+
     return (
         <div className = "timer">
             <h1 id="timeDisplay">{formatTime(time)}</h1>
             <div className="buttons">
-            <button type="button" id="lap" onClick={flipLapButton}>{resetLapButtonDisplay.current}</button>
-            <button type="button" id="toggle" onClick={flipToggleButton}>{startStopButtonDisplay.current}</button>
+            <button type="button" id="lap" onClick={isOn ? lap : reset}>{isOn ? "Lap" : "Reset"}</button>
+            <button type="button" id="toggle" onClick={flipToggle}>{isOn ? "Stop" : "Start"}</button>
             </div>
             <div className="laps">
                 <h2 className="lap-times-label">Lap times</h2>
-                <ul className="lap-list" id="lapTimes">
-                <li className="lap-times">{}</li></ul>
+                {lapDisplay.current}
             </div>
         </div>
     )
