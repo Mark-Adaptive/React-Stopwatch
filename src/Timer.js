c9f914b9b47aa0530f25c228d2cee0ab12fe08ee
Mounir Hamdar <mounir.hamdar@weareadaptive.com>
2020-06-18 10:49:09 -0400

    Refactor hook

--- src/Timer.js
+++ src/Timer.js
@@ -1,49 +1,22 @@
 import React, { useEffect, useRef, useMemo, useReducer } from 'react';
 import {msToTimerString, formatLap} from "./utils.js";
-import { initialStopwatch, reducerWithLaps } from "./useStopwatch.js"
+import { useStopwatch } from "./useStopwatch.js"
 
 function Timer() {
 
-    const [{elapsedTime, isRunning, lapTimes}, dispatch] = useReducer(reducerWithLaps, initialStopwatch);
+    const [{elapsedTime, isRunning}, dispatch] = useStopwatch();
     //const [lapTimes, lapDispatch] = useReducer(lapReducer, initialLaps);
-    const startTime = useRef(0);
-    
-    let timerProcess;
-    useEffect (() => {
-        if (isRunning) {
-            startTime.current += Date.now();
-            timerProcess = setInterval(() => {
-                dispatch({type: "TICK", payload: Date.now() - startTime.current});
-            }, 1)
-        }
-
-        else if (startTime.current !== 0) {
-            startTime.current -= Date.now();
-        }
-
-        return () => clearInterval(timerProcess);
-
-    }, [isRunning]);
-
-    // At reset
-    useEffect(() => {
-        if (elapsedTime === 0) {
-            startTime.current = 0;
-        }
-    }, [elapsedTime]);
     
     // When a new lap gets added
-    const lapDisplay = useMemo(() => {
-        console.log(lapTimes);
+    /*const lapDisplay = useMemo(() => {
         const minLapTime = lapTimes.length < 2 ? null : Math.min(...lapTimes);
         const maxLapTime = lapTimes.length < 2 ? null : Math.max(...lapTimes);
 
         return [...lapTimes].map((lap, index) => formatLap(lap, index, lapTimes.length, minLapTime, maxLapTime));
 
-    }, [lapTimes]);
+    }, [lapTimes]);*/
 
     return (
-        <body>
         <div className="App">
             <header className="App-header">
                 <div>
@@ -52,11 +25,11 @@ function Timer() {
                         <button id="toggle" onClick={() => dispatch({type: "TOGGLE"})}>{isRunning ? "Stop" : "Start"}</button>
                         <button id="lap" onClick={() => isRunning ? dispatch({type: "LAP", payload: elapsedTime}) : dispatch({type: "RESET"})}>{isRunning ? "Lap" : "Reset"}</button>
                     </div>
-                        <ul className="laps"> {lapDisplay} </ul>
+                    <div className="laps">
+                    </div>
                 </div>
             </header>
         </div>
-        </body>
     )
 }
 
--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -1,27 +1,6 @@
-export const initialStopwatch = {
-    elapsedTime: 0,
-    isRunning: false,
-    lapTimes: []
-}
+import React, { useEffect, useRef, useMemo, useReducer } from 'react';
 
-export const initialLaps = {
-    lapTimes: []
-}
-
-export const reducer = (state, {type, payload}) => {
-    switch(type) {
-        case "TOGGLE":
-            return {...state, isRunning: !state.isRunning};
-        case "TICK":
-            return {...state, elapsedTime: payload};
-        case "RESET":
-            return {...initialStopwatch, elapsedTime: 0};
-        default:
-            return state;
-    }
-}
-
-export const reducerWithLaps = (state, {type, payload}) => {
+/*export const reducerWithLaps = (state, {type, payload}) => {
     switch(type) {	
         case "LAP":	
             const timeCountedInLaps = state.lapTimes.reduce((sum, lapTime) => sum + lapTime, 0);
@@ -32,4 +11,76 @@ export const reducerWithLaps = (state, {type, payload}) => {
             return {elapsedTime, isRunning, lapTimes};
     }	
 }
+*/
 
+export const useStopwatch = () => {
+    const initialStopwatch = {
+        elapsedTime: 0,
+        isRunning: false,
+    }
+
+    const startTime = useRef(0);
+
+    function reducer (state, {type, payload}) {
+        switch(type) {
+            case "TOGGLE":
+                return {...state, isRunning: !state.isRunning};
+            case "TICK":
+                return {...state, elapsedTime: payload};
+            case "RESET":
+                return {...initialStopwatch, elapsedTime: 0};
+            default:
+                return state;
+        }
+    }
+
+    const [{elapsedTime, isRunning}, dispatch] = useReducer(reducer, initialStopwatch);
+
+    let timerProcess;
+    useEffect (() => {
+        if (isRunning) {
+            startTime.current += Date.now();
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            timerProcess = setInterval(() => {
+                dispatch({type: "TICK", payload: Date.now() - startTime.current});
+            }, 1)
+        }
+
+        else if (startTime.current !== 0) {
+            startTime.current -= Date.now();
+        }
+
+        return () => clearInterval(timerProcess);
+
+    }, [isRunning]);
+
+    // At reset
+    useEffect(() => {
+        if (elapsedTime === 0) {
+            startTime.current = 0;
+        }
+    }, [elapsedTime]);
+
+    return [{elapsedTime, isRunning}, dispatch];
+}
+
+
+/* const useStopwatch = () => {
+    // TODO: implement this
+
+    return [{]}]
+}
+
+
+const useStopwatchWithLaps = () => {
+    const [{ellapsedTime, isRunning}, stopwatchDispatch] = useStopwatch()
+    //
+    //
+    
+}
+*/
+
+/*
+type UseStopwatch: (): [{ellapsedTime: number, isRunning: boolean}, dispatch('TOGGLE' | 'RESET')]
+type UseStopwatch: (): [{ellapsedTime: number, isRunning: boolean, laps: number[]}, dispatch('TOGGLE' | 'RESET' | 'LAP')]
+*/
